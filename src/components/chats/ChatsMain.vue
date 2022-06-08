0e67c52b529b830309730f17908ae26268c5b703
Daisy Xu <daisy@Daisys-MBP.localdomain>
2022-06-08 16:44:10 -0700

    add files

---
+++ src/components/chats/ChatsMain.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+import { useMainStore } from '../../stores/mainStore'
+
+
+</script>
+
+<template>
+
+<div id="wrapper">
+
+    <div id="header">  
+    </div>
+
+    <div id="content">
+        
+    </div>
+
+</div>
+
+</template>
+
+<style scoped>
+
+#wrapper {
+    width: 100%;
+    height: 100%;
+
+    border-left: 1px solid #b8b7b7;
+
+    background-color: rgb(229, 229, 247);
+
+    background-image: repeating-radial-gradient( circle at 0 0, transparent 0, #e5e5f7 10px ), repeating-linear-gradient( rgba(242, 193, 139, 0.33), rgb(242, 193, 139, 0.4) );
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+}
+
+#header {
+    flex: 0 0 50px;
+    background-color: #f0f2f5;
+    padding: 10px;
+}
+
+#content {
+    flex: 1 1 auto;
+}
+
+</style>
