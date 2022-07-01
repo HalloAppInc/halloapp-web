e7f795b99ddcb6cbe125bcae675ebc31f2c379a2
Tony Jiang <tony@halloapp.com>
2022-07-01 12:29:36 -0700

    Comments: Auto switch to relevant comments panel when scrolling

--- src/components/home/HomeMain.vue
+++ src/components/home/HomeMain.vue
@@ -11,70 +11,110 @@ const mainStore = useMainStore()
 const listBoxWidth = ref('100%')
 const showComments = ref(false)
 
+const inViewPostID = ref('')
+
+const content = ref<HTMLElement | null>(null)
+let handleScrollTimer: any
+
 const listData = [
     { 
         userID: "xxx",
         title: "Thou with no name",
         subtitle: "this is a link",
         timestamp: "now",
+        postID: "1"
     },
     { 
         userID: "xxx",
         title: "Tree",
         subtitle: "apple",
         timestamp: "now",
+        postID: "2"
     },
     { 
         userID: "xxx",
         title: "Bob",
         subtitle: "this is a link",
         timestamp: "now",
+        postID: "3"
     },     
     { 
         userID: "xxx",
         title: "Jessy",
         subtitle: "this is a link",
         timestamp: "now",
+        postID: "4"
     },
     { 
         userID: "xxx",
         title: "Nathan",
         subtitle: "this is a link",
         timestamp: "now",
+        postID: "5"
     },
     { 
         userID: "xxx",
         title: "Kai",
         subtitle: "this is a link",
         timestamp: "now",
+        postID: "6"
     },
     { 
         userID: "xxx",
         title: "Rebecca",
         subtitle: "this is a link",
         timestamp: "now",
+        postID: "7"
     },     
     { 
         userID: "xxx",
         title: "Dylan",
         subtitle: "this is a link",
         timestamp: "now",
+        postID: "8"
     },          
 ]
 
-function commentsClick() {
+function commentsClick(postID: string) {
     if (listBoxWidth.value == '100%') {
         let width = '500px'
         if (mainStore.isMobile) {
             width = '0px'
         }
         listBoxWidth.value = width
+
+        inViewPostID.value = postID
         showComments.value = true
         mainStore.homePanel = 'comments'
     } else {
         listBoxWidth.value = '100%'
+
+        inViewPostID.value = ''
         showComments.value = false
         mainStore.homePanel = ''
+    }
+}
+
+function handleScroll() {
+    clearTimeout(handleScrollTimer)
+    handleScrollTimer = setTimeout(debouncedHandleScroll, 250)
+}
+
+function debouncedHandleScroll() {
+    if (!content.value) { return }
+
+    const contentViewportRect = content.value.getBoundingClientRect()
+    const pointX = (contentViewportRect.width / 2) + contentViewportRect.left
+    const pointY = contentViewportRect.top + 100
+    
+    const closestTopElement = document.elementFromPoint(pointX, pointY)
+    const closestElement = closestTopElement?.closest('[data-ha-postID]')
+
+    if (closestElement) {
+        const attr = closestElement.getAttribute('data-ha-postID')
+        if (attr) {
+            inViewPostID.value = attr
+        }
     }
 }
 
@@ -84,20 +124,23 @@ function commentsClick() {
 
 <div class="wrapper">
     
-    
-    <div class="listBox"> 
+    <div class="listBox" ref='content' @scroll='handleScroll()'>
         <div v-for="value in listData" class="container">
-            <Post userID="value.userID" @commentsClick="commentsClick"></Post>
+            <!-- data-ha-postID is used only for detecting post while scrolling -->
+            <Post 
+                :postID="value.postID"
+                userID="value.userID" 
+                @commentsClick="commentsClick(value.postID)" 
+                :data-ha-postID="value.postID"> 
+            </Post>
         </div>
     </div>
  
-    
     <div v-if="showComments" class="comments">
-        <Comment @backClick="commentsClick"></Comment>
+        <Comment :postID='inViewPostID' @backClick="commentsClick"></Comment>
     </div>
   
 </div>
-   
 
 </template>
 
@@ -117,9 +160,6 @@ function commentsClick() {
     border: 0px solid white;  /* creates padding around scroll thumb */
 }
 
-
-
-
 .wrapper {
     width: 100%;
     height: 100%;
@@ -127,8 +167,6 @@ function commentsClick() {
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
-
-    
 }
 
 .listBox {
@@ -139,7 +177,6 @@ function commentsClick() {
     overflow-x: hidden;
 
     transition: flex 300ms ease-in-out;
-    
 }
 
 .comments {
