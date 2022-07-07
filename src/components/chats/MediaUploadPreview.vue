4ce66bc9cd3c3c40f851ced6c24b6223ef453c00
llapfim <llapfim@gmail.com>
2022-07-06 22:57:50 -0700

    Create seperate file for media upload and download

--- src/components/chats/MediaUploadPreview.vue
+++ src/components/chats/MediaUploadPreview.vue
@@ -4,10 +4,15 @@ import { ref, computed, watch } from 'vue'
 import { useColorStore } from '../../stores/colorStore'
 import { useMainStore } from '../../stores/mainStore'
 
+import { useHAMediaUpload } from '../../composables/haMediaUpload'
+
 import InputBox from './InputBox.vue'
+import { arrayBuffer } from 'stream/consumers'
 
 const colorStore = useColorStore()
 const mainStore = useMainStore()
+
+const { uploadAndDownLoad } = useHAMediaUpload()
 
 const props = defineProps(['uploadFiles', 'messageList', 'contactList'])
 
@@ -45,63 +50,13 @@ const iconColor = computed(() => {
     return colorStore.icon
 })
 
-async function sendMediaToServer() {
-    const putUrl = `https://us-e-halloapp-media.s3-accelerate.dualstack.amazonaws.com/ZDg0YjI1ODQtZmQ3My0xMWVjLWE2NGYtMTIyNDZjYWZiNjcz?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAVF4QDF4XNVQY4HH4%2F20220706%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220706T213733Z&X-Amz-Expires=604800&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEJr%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJHMEUCIEKta%2FC8MD9nzLs5F2CkiMt9Rc9pHB%2Fo469AR5eHcJ4jAiEAku8eU3PUz%2BUOTgz6pUqQfhoX1qNUsUtooG1X%2FTpTS3Qq3AQIw%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARACGgwzNTYyNDc2MTMyMzAiDJWPckzVa7OnZhvWzyqwBN6e8dRyvGbOPbHq%2FKL0eIZSCCsNEMWvSsVP5%2BigyaXfqA4i8GuqqVp8t%2FEz6zG5W9Y1ADZ0tWKCwPfJ1BPlUXcSmD0tnkF5groXYzqEw1tIU7QuoBTzt%2BsI3YjaqHnY00PiscdJk5GoyG%2FrBhLpBW7tXqV9mEowUZu6KGXiR6UwPxx8lx4XauVxrZrfQWpb6KR%2FAHRsvniSHcQDvHwr8P%2FnCvweCvyJdFRZECs5xGQBcYyfPb2nnWl08dUEaNBiW138fR%2F6jAk7YC%2BNyseP%2B4kSaIohAuROeawa3%2FHjJVuPffD1UTtqsjq2xPgc6Qk5WNoVmU8Y%2B1OXpmHYoCEeTSIxQCXLnFz%2FLa8UXppb9mZw9ieaKv8buS8nWzm3kY1stZT94nNwf2XDc02P94o1GSCZSXo20un2kzwhZ07ZNebPajGKYpzrBFpqe53wYDoo3CaFjgREJ1HQ1%2Bjon6TElAAC7vw6vWd0fUMDzMpLKIqIePOWou0BP1czo16O06lX%2BeoERfZSdul9Iko7qwGgAV%2FGJl87zOhJLnEnJlqTiUpTlHB1tII4Vyx26jUDYhw75%2BCYqU%2F0kzf2Ht3AwPg2%2FyDCRKnAvPX9gqoefTg9CZCj6xsbyQ%2BszDG3rWVqMn2%2F28l1R2mQO76L8cpnHuHcIy8eQO8Qk5%2FoO%2Fj3xd7Ij81bX%2BK703OL0vC8Fx8XLKGaxfIVnqyMK6YQH2f%2B26Cp8krR3RZH0HlwIpL0xwIlZlErMNGSl5YGOqkBeRNaop3GsneqDP4LEpTiiuWnALLWskG%2FXaLtiuAuLvR0lWfh6cbS8Gpbmz9iMdmD6GsTq27ulo22ppDsYyP9bW1dSxaYS2pvRMAKVVIXu1LcnXpyA%2BweV0AtOPIMmQt36Hfe9j0dxrafs4e%2BkjXPO9L6%2BjuXATdPWO0nw975KFZNiXUlmJs2eTb1xiR3pWq7dz4nQ%2FzD%2FqEq8%2Bg56pTevlEtyOBM25Ygfw%3D%3D&X-Amz-SignedHeaders=host&X-Amz-Signature=71c2b0451e61e4f3b90da3492317157ac5089f2a7001589eae3b5b35c74f969f`
-    console.log('upload file size', props.uploadFiles[0].size)
-
-    let response = await fetch(mainStore.devCORSWorkaroundUrlPrefix + putUrl,
-        {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: props.uploadFiles[0]
-        })
-        .then(response => {
-            console.log('PUT status =', response.status)
-        })
-
-    // keep trying while succeed
-    /* while (response.status !== 200) {
-        let response = await fetch(mainStore.devCORSWorkaroundUrlPrefix + putUrl,
-            {
-                method: 'PUT',
-                body: props.uploadFiles[0]
-            }).then(response => {
-                console.log(response.status)
-            })
-    } */
-}
-
-async function getMediaFromServer() {
-    const getUrl = `https://u-cdn.halloapp.net/ZDg0YjI1ODQtZmQ3My0xMWVjLWE2NGYtMTIyNDZjYWZiNjcz`
-
-    const request = new Request(mainStore.devCORSWorkaroundUrlPrefix + getUrl)
-    const response = await fetch(request)
-    
-    let mediaBlob: Blob = await response.blob()
-    console.log('download file size', mediaBlob.size)
-    const mediaBlobUrl = URL.createObjectURL(mediaBlob)
-    attachMediaList.value.push(mediaBlobUrl)
-
-    /* while (response.status !== 200) {
-        let response = await fetch(mainStore.devCORSWorkaroundUrlPrefix + getUrl,
-            {
-                method: 'GET',
-            })
-            .then(response => response.blob())
-            .then(function (myBlob) {
-                var objectURL = URL.createObjectURL(myBlob)
-                attachMediaList.value.push(url)
-            })
-    } */
-}
-
-async function uploadAndDownLoad() {
-    await sendMediaToServer()
-    await getMediaFromServer()
-    console.log("1", attachMediaList.value[0])
-    test.value.src = attachMediaList.value[0]
+async function testUploadAndDownload(file: any) {
+    await uploadAndDownLoad(file, attachMediaList.value)
+    // wait for uploadAndDownLoad
+    setTimeout(() => {
+        // console.log(attachMediaList.value)
+        test.value.src = attachMediaList.value[attachMediaList.value.length - 1]
+    }, 5000)
 }
 </script>
 
@@ -121,26 +76,27 @@ async function uploadAndDownLoad() {
             <!-- tool box: edit uploaded photo -->
             <div id='header'>
                 <!-- test upload and download -->
-                <div class='iconContainer' @click='uploadAndDownLoad'>
+                <div class='iconContainer'>
                     <div class='iconShadow'>
                         <font-awesome-icon :icon="['fas', 'face-smile']" size='lg' />
                     </div>
                 </div>
-                <img ref='test' />
+
+                <div class='button' @click='testUploadAndDownload(props.uploadFiles[0])'> try upload and download </div>
             </div>
 
             <!-- image box: show the image -->
             <div id='content'>
                 <div class='imgContainer'>
                     <img :src='mediaUrlList[0]' />
+                    <img ref='test' />
                 </div>
             </div>
 
             <!-- input box: add caption -->
             <div id='footer'>
                 <div class='chatBoxTray' ref='chatBox'>
-                    <InputBox :message-list='messageList' :contact-list='contactList'
-                        :upload-files='mediaUrlList[0]'/>
+                    <InputBox :message-list='messageList' :contact-list='contactList' :upload-files='mediaUrlList[0]' />
                 </div>
             </div>
         </div>
@@ -220,5 +176,15 @@ img {
     margin-bottom: 100px;
     display: flex;
     flex-direction: row;
+}
+
+.button {
+    margin: 10px 0px;
+    width: 200px;
+    height: 30px;
+    background-color: azure;
+    border: 1px solid aqua;
+    border-radius: 10px;
+    text-align: center;
 }
 </style>
---
+++ src/composables/haMediaUpload.ts
@@ -0,0 +1,62 @@
+import { useMainStore } from '../stores/mainStore'
+import { useConnStore } from '../stores/connStore'
+
+export function useHAMediaUpload() {
+
+    const mainStore = useMainStore()
+    const connStore = useConnStore()
+
+    async function sendMediaToServer(file: any, putUrl: string) {
+
+        let status = -1
+    
+        while (status !== 200) {
+            let response = await fetch(mainStore.devCORSWorkaroundUrlPrefix + putUrl,
+                {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    body: file
+                })
+    
+            status = response.status
+            // console.log('PUT status =', response.status)
+        }
+    }
+
+    async function getMediaFromServer(getUrl: string) {
+
+        const request = new Request(mainStore.devCORSWorkaroundUrlPrefix + getUrl)
+    
+        let status = -1
+        let response: any
+    
+        while (status !== 200) {
+            response = await fetch(request)
+            status = response.status
+        }
+    
+        let mediaBlob: Blob = await response.blob()
+        const mediaBlobUrl = URL.createObjectURL(mediaBlob)
+        return mediaBlobUrl
+    }
+
+    async function uploadAndDownLoad(file: any, list: any) {
+        // if the file is valid
+        if (file) {
+            connStore.getMediaUrl(1000, async function (val: any) {
+                // upload
+                await sendMediaToServer(file, val.iq?.uploadMedia?.url?.put)
+                // download
+               await getMediaFromServer(val.iq?.uploadMedia?.url?.get).then(
+                    (res) => {
+                        list.push(res)
+                    }
+                )
+            })
+        }
+    }
+
+    return { uploadAndDownLoad }
+}
