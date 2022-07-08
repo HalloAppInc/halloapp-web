dc5917499adfa0ad3f2743c718c4f6605751bdf7
llapfim <66579500+llapfim@users.noreply.github.com>
2022-07-08 14:16:46 -0700

    Add send button in inputBox (#39)
    1. Add send button, and set button color to blue

--- src/components/chats/InputBox.vue
+++ src/components/chats/InputBox.vue
@@ -27,8 +27,13 @@ const disableUpdate = ref(false)
 const currentNode = ref(<Node | null>(null))
 const nodeOffset = ref(0)
 
+const showSendButton = ref(false)
+
 const chatBoxHeight = ref(0)
 
+const iconColor = computed(() => {
+    return colorStore.icon
+})
 const headerColor = computed(() => {
     return colorStore.header
 })
@@ -67,7 +72,7 @@ function analyzeKeyDown(e: any) {
             totalOffset = cursorPosition.value
             currentNode.value = null
             getChildNodeAndOffsetFromNestedNodes(inputArea.value, false)
-            if (currentNode.value){
+            if (currentNode.value) {
                 const node = currentNode.value as HTMLElement
                 if (node &&
                     node.parentElement?.nodeName == 'SPAN' &&
@@ -128,8 +133,10 @@ function sendMessage() {
             quoteIdx: mainStore.chatPage.includes('reply') ? mainStore.chatPage.substring(5) : -1, // get reply id
             type: 'outBound',
             message: processText(inputArea.value?.innerText.trim(), props.contactList).html,
-            timestamp: Date.now() / 1000 | 0, //  get current time
+            timestamp: Date.now() / 1000 | 0, // get current time
         })
+        // hide send button
+        showSendButton.value = false
         // clean text inside input box
         if (inputArea.value) {
             inputArea.value.innerText = ''
@@ -200,8 +207,15 @@ function analyzeKeyUp(e: any) {
 
     // if inputArea is empty, delete the remaining <span> and <div>
     if (inputArea.value && inputArea.value?.innerText.trim().length == 0) {
+        // clean html inside
         inputArea.value.innerHTML = ''
+        // hide send button
+        showSendButton.value = false
         return
+    }
+    else {
+        // have valid input show send button
+        showSendButton.value = true
     }
 
     if (e.keyCode != 16 && // Shift
@@ -444,6 +458,11 @@ function checkContacts() {
         <!-- <div class='iconContainer'>
             <font-awesome-icon :icon="['fas', 'microphone']" size='2x' />
         </div> -->
+        <div class='buttonContainer' v-show='showSendButton' @click='sendMessage()'>
+            <div class='buttonIconContainer'>
+                <font-awesome-icon :icon="['fas', 'paper-plane']" size='lg' />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -593,5 +612,34 @@ function checkContacts() {
     white-space: nowrap;
     user-select: none;
     overflow: hidden;
+}
+
+.buttonContainer {
+    height: 40px;
+    width: 40px;
+    margin: 5px;
+    border-radius: 100%;
+    background-color: #007AFF;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2);
+
+    align-content: center;
+
+    display: flex;
+    flex-direction: horizontal;
+
+    z-index: 2;
+}
+
+.buttonContainer:hover {
+    cursor: pointer;
+}
+
+.buttonIconContainer {
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFFFFF;
 }
 </style>
