3edadebbc90ea18ffb74498f44f6357423170b36
llapfim <llapfim@gmail.com>
2022-08-05 16:54:40 -0700

    Updates

--- src/components/settings/settingsSidebar.vue
+++ src/components/settings/settingsSidebar.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 import { useI18n } from 'vue-i18n'
 
 import MainMenu from './Main.vue'
+import HelpMenu from './Help.vue'
+import NotificationsMenu from './Notifications.vue'
+import PrivacyMenu from './Privacy.vue'
+import SecurityMenu from './Security.vue'
 
 import { useColorStore } from '../../stores/colorStore'
+import { useMainStore } from '../../stores/mainStore'
 
 const colorStore = useColorStore()
+const mainStore = useMainStore()
 
 const { t } = useI18n({
     inheritLocale: true,
@@ -17,17 +23,60 @@ const { t } = useI18n({
 const backgroundColor = computed(() => {
     return colorStore.background
 })
+const boderlineColor = computed(() => {
+    return colorStore.borderline
+})
 
+const settingsSidebar = ref<HTMLDivElement>()
+
+// find the offset to the top
+const offsetTop = computed(() => {
+    if (settingsSidebar.value) {
+        console.log('*******',settingsSidebar.value.offsetTop)
+        let offset = settingsSidebar.value.clientHeight / 2
+        console.log(offset)
+        return -1 * offset
+    }
+})
 </script>
 
 
 <template>
-    <div class="wrapper">
-        <MainMenu />
-    </div>
+    <Transition name='settings'>
+        <div class="wrapper" ref='settingsSidebar'>
+            <MainMenu />
+            <PrivacyMenu />
+            <NotificationsMenu />
+            <HelpMenu />
+            <SecurityMenu />
+        </div>  
+    </Transition>
 </template>
 
 <style scoped>
+.settings-enter-active {
+    transition: all 0.15s;
+}
+
+.settings-leave-active {
+    transition: all 2s;
+}
+
+.settings-enter-from {
+    transform: translateX(-200px);
+    opacity: 0;
+}
+
+.settings-leave-from {
+    transform: translateY(-353px);
+    opacity: 1;
+}
+
+.settings-leave-to {
+    transform: translateX(-200px) translateY(-353px);
+    opacity: 0;
+}
+
 *::-webkit-scrollbar {
     width: 5px;
 }
@@ -52,5 +101,7 @@ const backgroundColor = computed(() => {
     justify-content: flex-start;
 
     overflow: hidden;
+
+    border-right: 1px solid v-bind(boderlineColor);
 }
 </style>
